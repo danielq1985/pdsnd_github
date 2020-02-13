e385287970fccd1d9566580e22a46948b3cb6691
danielq1985 <danielq1985@gmail.com>
2020-02-12 18:20:18 -0700

    Remove starter code instructions for time_status, station_stats, trip_duration, and user_stats functions

--- bike_share.py
+++ bike_share.py
@@ -56,7 +56,7 @@ def load_data(city, month, day):
 
 
 def time_stats(df):
-    """Displays statistics on the most frequent times of travel."""
+
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
@@ -79,7 +79,7 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
@@ -98,7 +98,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
@@ -114,7 +114,7 @@ def trip_duration_stats(df):
 
 
 def user_stats(df):
-    """Displays statistics on bikeshare users."""
+
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
